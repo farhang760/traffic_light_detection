334d849c1206de77cf80ff2bb8cfc635876e5316
Ehsan <malayjerdi@gmail.com>
2022-09-30 15:43:14 +0300

    initial commit

---
+++ src/Traffic_light_detector.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String, Bool, Float32
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import darknet_detector
+import darknet_library
+import cv2
+
+class TrafficLightNode:
+    def __init__(self):
+        rospy.init_node("traffic_light_detector", anonymous=True)
+
+        # Initializers
+        self.bridge = CvBridge()
+        self.video_location = rospy.get_param("/video_address")
+        self.yolo_config = rospy.get_param("/config_address")
+        self.yolo_weight = rospy.get_param("/weight_address")
+        self.yolo_data = rospy.get_param("/data_address")
+  
+   
+
+        
+        # Class variables 
+        self.darknet_config = darknet_library.load_net(self.yolo_config, self.yolo_weight, 0)
+        self.darknet_data = darknet_library.load_meta(self.yolo_data)
+
+        
+        
+        
+   
+     
+
+        # Publishers
+        self.publish_detected_light = rospy.Publisher("traffic_light_detected",Bool,queue_size=10)
+        self.publish_traffic_size = rospy.Publisher("traffic_light_size", Vector3, queue_size=10)
+        self.publish_zone_height = rospy.Publisher("zone_height",Float32, queue_size=10)
+
+        # Subscribers
+        #self.image_sub = rospy.Subscriber("/cam/image_raw", Image, self.__CameraCallback)
+
+    def reading_video(self):
+       
+        vector3_size = Vector3()
+
+
+        try:
+            
+            video_source = cv2.VideoCapture(self.video_location)
+        except CvBridgeError as e:
+            rospy.loginfo(e)
+
+        
+        while(video_source.isOpened()):
+
+            ret, cv_image = video_source.read()
+            if ret == False:
+                break
+            resized_image = cv2.resize(cv_image,(800,600))
+            detected_obj = darknet_detector.detect(self.darknet_config, self.darknet_data, resized_image)
+
+            if len(detected_obj) and 'traffic light' in detected_obj[0]:
+                x, y, w, h = detected_obj[0][2][0], detected_obj[0][2][1], detected_obj[0][2][2], detected_obj[0][2][3]
+                vector3_size.x = w
+                vector3_size.y = h
+                vector3_size.z = 0
+                self.publish_detected_light.publish(True)
+                self.publish_traffic_size.publish(vector3_size)
+                zone_height = vector3_size.y/ 3
+                self.publish_zone_height.publish(zone_height)
+
+
+            cv2.imshow("IMAGE", resized_image)
+            cv2.waitKey(1)
+
+
+
+        
+def main():
+    TrafficLightNode().reading_video()
+    node = TrafficLightNode()
+    rospy.sleep(0.5)
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        TrafficLightNode().reading_video()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/darknet_class.py
@@ -0,0 +1,93 @@
+
+from ctypes import *
+import math
+import random
+import cv2
+import time
+import numpy as np
+
+
+def sample(probs):
+    s = sum(probs)
+    probs = [a/s for a in probs]
+    r = random.uniform(0, 1)
+    for i in range(len(probs)):
+        r = r - probs[i]
+        if r <= 0:
+            return i
+    return len(probs)-1
+
+def c_array(ctype, values):
+    arr = (ctype*len(values))()
+    arr[:] = values
+    return arr
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int)]
+
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+
+
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def array_to_image(arr):
+    # need to return old values to avoid python freeing memory
+    arr = arr.transpose(2,0,1)
+    c, h, w = arr.shape[0:3]
+    arr = np.ascontiguousarray(arr.flat, dtype=np.float32) / 255.0
+    data = arr.ctypes.data_as(POINTER(c_float))
+    im = IMAGE(w,h,c,data)
+    return im, arr
+
+def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    im, image = array_to_image(image)
+    rgbgr_image(im)
+    num = c_int(0)
+    pnum = pointer(num)
+    predict_image(net, im)
+    dets = get_network_boxes(net, im.w, im.h, thresh,
+                             hier_thresh, None, 0, pnum)
+    num = pnum[0]
+    if nms: do_nms_obj(dets, num, meta.classes, nms)
+
+    res = []
+    for j in range(num):
+        a = dets[j].prob[0:meta.classes]
+        if any(a):
+            ai = np.array(a).nonzero()[0]
+            for i in ai:
+                b = dets[j].bbox
+                res.append((meta.names[i], dets[j].prob[i],
+                           (b.x, b.y, b.w, b.h)))
+
+    res = sorted(res, key=lambda x: -x[1])
+    if isinstance(image, bytes): free_image(im)
+    free_detections(dets, num)
+    return res
